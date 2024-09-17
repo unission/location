c014961263b24e451f652dfe1e3b7d6619b7be1a
Chenvel <105103515+Pussia@users.noreply.github.com>
2024-09-17 20:21:04 +0600

    Default Architecture

---
+++ src/main/java/org/unission/location2/Location2Application.java
@@ -0,0 +1,13 @@
+package org.unission.location2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Location2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Location2Application.class, args);
+    }
+
+}
